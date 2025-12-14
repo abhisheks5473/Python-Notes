56889e76ad7cc649488d74bed4fb5c0796691609
Abhishek singh <abhisheks5473@gmail.com>
2025-12-14 19:42:32 +0530

    Add files via upload

---
+++ Basic_operations_of_tuple.py
@@ -0,0 +1,52 @@
+"""
+concatenation, repetition, membership
+count, index
+min, max, sum
+"""
+student_detail1 = (1001, 'Jhon')
+student_detail2 = (78.5, 91.0, 83.5, 79.5)
+
+# concatenation (+)
+student_details = student_detail1 + student_detail2
+print(student_details)
+print()
+
+# repetition (*)
+t1 = ("Class 5", 5000)
+print(t1 * 3)
+print()
+
+# membership operation (in, not in) checks weather the item is present in the string, list, or tuple
+student_detail2 = (78.5, 91.0, 83.5, 79.5)
+print(91 in student_detail2) # if a float has .0 ending and the part that you asked is the part before the  decimal the answer will be true
+print(99 not in student_detail2)
+print()
+
+# count
+x= (10, 4, 1, 9, 0, 3, 1)
+#tuple.count(element)
+print(x.count(1))
+print()
+
+# index
+x= (10, 4, 1, 9, 0, 3, 1)
+#tuple.index(element)
+print(x.index(4)) # wht is index of 4 in tuple x
+print()
+
+# min, max, sum
+numbers = (10, 4, -1, 20, 5.5, 7, 1)
+
+# Smallest number in the list
+# min()
+print(f"Smallest number: {min(numbers)}")
+
+# Biggest number in the list
+# max()
+print(f"Biggest number: {max(numbers)}")
+
+# Total of the numbers in the list
+# sum()
+print(f"Total: {sum(numbers)}")
+
+
---
+++ tuple_intro.py
@@ -0,0 +1,40 @@
+'''
+# Tuple
+# (item1, item2, ....)
+# sequence of items as a collection
+# you can't change the items of tuple (modification operation), but you can do any read operation on the items of the tuple.
+
+
+
+t1 = ("Python", 10, 1.5, True, [1,2,4], (10,20))
+print(len(t1))
+print(type(t1))
+
+#Accessing items of a tuple - index
+print(t1[0])
+print(t1[-1])
+
+# Slicing in tuple
+print(t1[1:5:1])
+'''
+
+'''
+t1 = 10, 20 ,30 ,40 ,50 ,60 # this is also a tuple, it is not necessary for a tuple to have "()"
+print(t1)
+print(type(t1))
+'''
+
+'''
+l1 = [10, 'mango', 3.0]
+
+print(l1)
+print(type(l1))
+
+t1 = tuple(l1)
+print(t1)
+print(type(t1)) # you can change type list to type tuple and visa versa
+
+'''
+
+
+
